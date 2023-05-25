94d76e914ecb0c9c6923278af816ff31e20468bf
A4uster <113182551+A4uster@users.noreply.github.com>
2023-05-25 06:18:59 +0200

    1

--- Assets/wacho/scripts/Enemigo.cs
+++ Assets/wacho/scripts/Enemigo.cs
@@ -6,11 +6,9 @@ using UnityEngine.SceneManagement;
 public class Enemigo : MonoBehaviour
 {
     public float rangoArea; // Rango de detección del jugador
-    public float rangoAtaque; // Rango de ataque del jugador
     public LayerMask CapaPj; // Capa del jugador
     bool estarAlerta; // Indica si el enemigo está alerta o no
     public Transform Pj; // Transform del jugador
-    public Animator _anim; // Animator del enemigo
     public float vel; // Velocidad de movimiento del enemigo
     public int vida; // Vida del enemigo
 
@@ -28,30 +26,9 @@ public class Enemigo : MonoBehaviour
 
         if (estarAlerta)
         {
-            // Calcular la distancia al jugador
-            float distancia = Vector3.Distance(transform.position, Pj.position);
-
-            if (distancia <= rangoAtaque)
-            {
-                // Si el enemigo está dentro del rango de ataque, ejecutar animación de ataque
-                _anim.SetBool("attack", true);
-                _anim.SetBool("run", false);
-            }
-            else
-            {
-                _anim.SetBool("run", true);
-                _anim.SetBool("attack", false);
-                // Si el enemigo está alerta pero fuera del rango de ataque, rotar hacia el jugador y moverse hacia él
-                transform.LookAt(new Vector3(Pj.position.x, transform.position.y, Pj.position.z));
-                transform.position = Vector3.MoveTowards(transform.position, new Vector3(Pj.position.x, transform.position.y, Pj.position.z), vel * Time.deltaTime);
-                
-            }
-        }
-        else
-        {
-            // Si el enemigo no está alerta, detener animación de ataque y de correr
-            _anim.SetBool("attack", false);
-            _anim.SetBool("run", false);
+            // Si el enemigo está alerta, rotar hacia el jugador y moverse hacia él
+            transform.LookAt(new Vector3(Pj.position.x, transform.position.y, Pj.position.z));
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(Pj.position.x, transform.position.y, Pj.position.z), vel * Time.deltaTime);
         }
     }
 
