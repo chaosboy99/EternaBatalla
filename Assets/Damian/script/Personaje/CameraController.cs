db43fb267b11876998e096c35597d8067a967062
A4uster <113182551+A4uster@users.noreply.github.com>
2023-05-25 03:08:08 +0200

    correccion de gameOver finalizada

--- Assets/Damian/script/Personaje/CameraController.cs
+++ Assets/Damian/script/Personaje/CameraController.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
-    // Transform de la cámara
-    public Transform cameraTransform;
-
-    // Ángulo máximo de rotación vertical de la cámara
-    public float maxVerticalAngle;
-
     // Transform del cuerpo del jugador
     public Transform body;
 
@@ -38,30 +32,20 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    // Ángulo máximo de rotación vertical de la cámara
+    public float maxVerticalAngle;
+
     // Sensibilidad de rotación de la cámara
     public float sensitivity;
 
-    // Variable para almacenar el estado de la escena
-    private bool isGameOverOrWin;
+    // Indica si se está en la escena GameOver o Win
+    private bool isGameOverOrWinScene;
 
     // Método llamado en cada frame
     void Update()
     {
-        // Verifica si la escena actual es "gameover" o "win"
-        if (SceneManager.GetActiveScene().name == "gameover" || SceneManager.GetActiveScene().name == "win")
-        {
-            // Desactiva el movimiento de la cámara
-            isGameOverOrWin = true;
-            return;
-        }
-        else
-        {
-            // Activa el movimiento de la cámara
-            isGameOverOrWin = false;
-        }
-
-        // Si el movimiento de la cámara está desactivado, no se realiza ninguna acción
-        if (isGameOverOrWin) return;
+        // Si se está en la escena GameOver o Win, no se realiza ninguna acción
+        if (isGameOverOrWinScene) return;
 
         // Obtiene el valor vertical del movimiento del mouse
         MouseVerticalValue = Input.GetAxis("Mouse Y");
@@ -70,7 +54,7 @@ public class CameraController : MonoBehaviour
         Quaternion finalRotation = Quaternion.Euler(-MouseVerticalValue * sensitivity, 0, 0);
 
         // Aplica la rotación a la transformación de la cámara
-        cameraTransform.localRotation = finalRotation;
+        transform.localRotation = finalRotation;
 
         // Rota el cuerpo horizontalmente en base al movimiento horizontal del mouse
         body.rotation = Quaternion.Euler(0, body.localRotation.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivity, 0);
@@ -78,7 +62,7 @@ public class CameraController : MonoBehaviour
         // Bloquea el cursor y lo oculta al hacer clic derecho
         if (Input.GetMouseButtonDown(0))
         {
-            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.lockState = CursorLockMode.None;
             Cursor.visible = false;
         }
 
@@ -88,5 +72,11 @@ public class CameraController : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
+    }
+
+    // Método para cambiar el estado de la escena GameOver o Win
+    public void SetGameOverOrWinScene(bool value)
+    {
+        isGameOverOrWinScene = value;
     }
 }
--- Assets/Damian/script/Scene Transitiion/sceneTransition.cs
+++ Assets/Damian/script/Scene Transitiion/sceneTransition.cs
@@ -10,6 +10,17 @@ public class sceneTransition : MonoBehaviour
 
     public void OnTriggerEnter(Collider collision)
     {
+        // Reinicia o desactiva el componente CameraController
+        CameraController cameraController = FindObjectOfType<CameraController>();
+        if (cameraController != null)
+        {
+            // Reiniciar el componente
+            // cameraController.Restart(); // Reemplaza "Restart()" con el método adecuado para reiniciar el componente CameraController
+
+            // Desactivar el componente
+            // cameraController.enabled = false;
+        }
+
         // Carga la escena especificada por el número de índice "Escena".
         SceneManager.LoadScene(Escena);
     }
